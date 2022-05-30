b171fdb591443a4e314cd5669bdc09e41c440e18
Matthew Hasler <matthew.hasler@thetrainline.com>
2022-05-30 20:31:43 +0100

    Added ItemUpdater protocol.

--- swift/Sources/GildedRose/Item+UpdateQuality.swift
+++ swift/Sources/GildedRose/Item+UpdateQuality.swift
@@ -1,53 +1,11 @@
 extension Item {
     
     func updateQuality() {
-    
-        sellIn = calculateNewSellInValue()
-        quality = calculateNewQualityValue()
-            .clamping(min: 0, max: 50)
+        itemUpdater.update(item: self)
     }
     
-    private func calculateNewSellInValue() -> Int {
-        isSulfuras ? sellIn : sellIn - 1
-    }
-    
-    private func calculateNewQualityValue() -> Int {
-        
-        var newQuality = quality
-        
-        if !isAgedBrie && !isBackstagePasses {
-            if !isSulfuras {
-                newQuality = newQuality - 1
-            }
-        } else {
-            newQuality = newQuality + 1
-            
-            if isBackstagePasses {
-                if sellIn < 10 {
-                    newQuality = newQuality + 1
-                }
-                
-                if sellIn < 5 {
-                    newQuality = newQuality + 1
-                }
-            }
-        }
-        
-        if isPastSellByDate {
-            if !isAgedBrie {
-                if !isBackstagePasses {
-                    if !isSulfuras {
-                        return newQuality - 1
-                    }
-                } else {
-                    return 0
-                }
-            } else {
-                return newQuality + 1
-            }
-        }
-        
-        return newQuality
+    var itemUpdater: ItemUpdater {
+        NormalItemUpdater()
     }
     
 }
